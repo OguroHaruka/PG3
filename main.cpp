472472a3745e409914b4d11ae2ca5bb995fd58e1
Oguro Haruka <Oguro Haruka@MSI>
2023-10-10 10:29:48 +0900

    01_01

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+template<typename Type>
+Type Min(Type a, Type b) {
+	if (a > b) {
+		return b;
+	}
+	else if (a < b) {
+		return a;
+	}
+}
+
+template<>
+char Min<char>(char a, char b) {
+	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+	return a;
+}
+
+int main(void) {
+	printf("%d\n", Min<int>(10, 20));
+	printf("%f\n", Min<float>(10.0f, 20.0f));
+	printf("%f\n", Min<double>(10.0f, 20.0f));
+	char a=10, b=20;
+	Min<char>(a, b);
+}
