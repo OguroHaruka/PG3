81fa4034c8325b7ac01e0fbc4cfafb373af2741c
Oguro Haruka <Oguro Haruka@MSI>
2023-10-31 11:38:33 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <windows.h>
+#include <time.h>
+#include <functional>
 
-int main(int argc, const char* argv[]) {
-	[]() {printf("lamda test"); }();
+typedef void (*PFunc)(int*);
+void WinResult(int* s) {
+	printf("“–‚½‚è\n");
+}
+void LoseResult(int* s) {
+	printf("‚Í‚¸‚ê\n");
+}
+
+void setTimeout(PFunc p, int second) {
+	Sleep(second * 1000);
+	p(&second);
+}
+
+int main(int argc,const char*argv[]) {
+	srand(time(nullptr));
+	int dice;
+	std::function<int()> diceNumber = [=]() {return dice + rand() % 6 + 1; };
+	int number;
+	[]() {printf("”¼‚©’š‚©?\n"); }();
+	scanf_s("%d", &number);
+	PFunc p;
+	if (diceNumber() % 2 == number % 2) {
+		p = WinResult;
+		setTimeout(p, 3);
+	}
+	else {
+		p = LoseResult;
+		setTimeout(p, 3);
+	}
 	return 0;
 }
