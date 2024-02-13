9d0bfa6db1de8333232bf4c145ce6e9aa835671a
Oguro Haruka <Oguro Haruka@MSI>
2024-02-13 22:01:38 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
 
-	list<const char*> station{
+	list<const char*> stationName{
 		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani", "Nippori","Tabata", "Komagome",
 		"Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Shin-Okubo","Shinjuku", "Yoyogi", "Harajuku", "Shibuya",
 		"Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa","Tamachi", "Hamamatutsucho", "Shimbashi", "Yurakucho",
@@ -14,32 +14,32 @@ int main() {
 
 	
 	printf("1970”N\n");
-	for (auto itr = station.begin(); itr != station.end(); ++itr) {
+	for (auto itr = stationName.begin(); itr != stationName.end(); ++itr) {
 		std::cout << *itr << "\n";
 	}
 	printf("\n");
 	
-	for (auto itr = station.begin(); itr != station.end(); ++itr) {
+	for (auto itr = stationName.begin(); itr != stationName.end(); ++itr) {
 		if (*itr == "Tabata") {
-			itr = station.insert(itr, "Nishi-Nippori");
+			itr = stationName.insert(itr, "Nishi-Nippori");
 			++itr;
 		}
 	}
 	printf("2019”N\n");
-	for (auto itr = station.begin(); itr != station.end(); ++itr) {
+	for (auto itr = stationName.begin(); itr != stationName.end(); ++itr) {
 		std::cout << *itr << "\n";
 	}
 	printf("\n");
 	
-	for (auto itr = station.begin(); itr != station.end(); ++itr) {
+	for (auto itr = stationName.begin(); itr != stationName.end(); ++itr) {
 		if (*itr == "Tamachi") {
-			itr = station.insert(itr, "Takanawa Gateway");
+			itr = stationName.insert(itr, "Takanawa Gateway");
 			++itr;
 		}
 	}
 
 	printf("2022”N\n");
-	for (auto itr = station.begin(); itr != station.end(); ++itr) {
+	for (auto itr = stationName.begin(); itr != stationName.end(); ++itr) {
 		std::cout << *itr << "\n";
 	}
 
